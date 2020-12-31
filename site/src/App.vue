6d4bcf29ca8e8f1426c4deec58b497a3f8863a91
Suhas-13 <hariharan774531@sas.edu.sg>
2020-12-31 23:48:56 +0800

    removed router

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <About> </About>
     </div>
-    <router-view/>
   </div>
 </template>
-
+<script>
+import About from './views/About.vue'
+export default {
+  name: 'Show',
+  components: {
+      About
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -16,16 +22,13 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
 }
